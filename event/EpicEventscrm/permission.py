f70ba867c52c7b35bd739422cda042253075b1ba
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-10-21 23:01:46 +0200

    Delete event directory

--- event/EpicEventscrm/permission.py
+++
@@ -1,6 +0,0 @@
-def allowed_users(user, allowed_roles: list) -> bool:
-    groups = [group.name for group in user.groups.all()]
-    for roles in allowed_roles:
-        if roles in groups:
-            return True
-    return False
